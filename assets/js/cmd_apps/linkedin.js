585adb4c4ff08f68fe869d28f07b928f75b05bdf
Julian Loscombe <48539740+lostcomb@users.noreply.github.com>
2019-03-18 22:23:15 +0000

    Use linkedin username from _config for cmd app

--- assets/js/cmd_apps/linkedin.js
+++ assets/js/cmd_apps/linkedin.js
@@ -5,6 +5,6 @@
 window.addEventListener("register_app", function (e) {
   e.detail.registerApplication("linkedin", function (output) {
     output.parentNode.removeChild(output);
-    window.location = "https://uk.linkedin.com/in/julian-loscombe-ab4385a8";
+    window.location = "https://uk.linkedin.com/in/{{ site.linkedin_username }}";
   }, "Re-directs to my Linkedin page.");
 });
